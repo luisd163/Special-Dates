dd25094a42afb6e8e1c90bca9389c8c3de5c7207
Luis Daniel <luisd.gomezv@uqvirtual.edu.co>
2025-11-21 12:16:30 -0500

    code

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module uniquindio.edu.co.specialdates {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens uniquindio.edu.co.specialdates to javafx.fxml;
+    exports uniquindio.edu.co.specialdates;
+}
